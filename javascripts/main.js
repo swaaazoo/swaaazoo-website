a1bd39b5021eb1bff24aab68257ff0e4bd32ec1c
Kris Lewallen <kris@swaaazoo.com>
2015-05-15 09:26:56 -0700

    updated frontend partial

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,32 +1,31 @@
+$(document).ready(function () {
+	$('.accordion-toggle').on('click', function(event){
+		event.preventDefault();
+		// create accordion variables
+		var accordion = $(this);
+		var accordionContent = accordion.next('.accordion-content');
+		// toggle accordion content
+		accordionContent.slideToggle(250);
+	});
 
-		$(document).ready(function () {
-		    $('.accordion-toggle').on('click', function(event){
-		    	event.preventDefault();
-		    	// create accordion variables
-		    	var accordion = $(this);
-		    	var accordionContent = accordion.next('.accordion-content');
-		    	// toggle accordion content
-		    	accordionContent.slideToggle(250);
-		    });
-
-		    $('.accordion-toggle').click(function() {
-		    	$('div.left_bottom').toggleClass('shift');
-		    });
-		// });
-		//  $(function() {
-		    // $( "#dialog" ).dialog({
-		    //   autoOpen: false,
-		    //   show: {
-		    //     effect: "blind",
-		    //     duration: 1000
-		    //   },
-		    //   hide: {
-		    //     effect: "explode",
-		    //     duration: 1000
-		    //   }
-		    // });
-		 
-		    // $( "#show" ).click(function() {
-		    //   $( "#dialog" ).dialog( "open" );
-		    // });
-		  });
+	$('.accordion-toggle').click(function() {
+		$('div.left_bottom').toggleClass('shift');
+	});
+// });
+//  $(function() {
+	// $( "#dialog" ).dialog({
+	//   autoOpen: false,
+	//   show: {
+	//     effect: "blind",
+	//     duration: 1000
+	//   },
+	//   hide: {
+	//     effect: "explode",
+	//     duration: 1000
+	//   }
+	// });
+ 
+	// $( "#show" ).click(function() {
+	//   $( "#dialog" ).dialog( "open" );
+	// });
+  });
